02ff8ba96d9a75d206b56e7bdc7c496cdfd15f11
omar-powell-guaglione <omar-powell-guaglione@users.noreply.github.com>
2020-10-23 16:41:45 +0200

    and crash again

--- website/js/site.js
+++ website/js/site.js
@@ -200,7 +200,7 @@ $(document).ready(function () {
         listView = document.getElementById('cbp-spmenu-s3'),
         showLeftPush = document.getElementById('showLeftPush'),
         showRightPush = document.getElementById('showRightPush'),
-        // showTimezoneClock = document.getElementById('showTimezoneClock'), // add
+        showTimezoneClock = document.getElementById('showTimezoneClock'), // add
         close_timezone_sidebar = document.getElementById('close_timezone_sidebar'), // add
         showLeft = document.getElementById('showLeft'),
         body = document.body;
@@ -227,20 +227,20 @@ $(document).ready(function () {
     };
 
     // display timezone sidebar
-    // showTimezoneClock.onclick = function () {
-    //     $(this).toggleClass('active');
-    //     $(body).toggleClass('fxs_push_timezone');
-    //     $(itemsTimezone).toggleClass('fxs_show_timezoneHours');
-    // };
+    showTimezoneClock.onclick = function () {
+        $(this).toggleClass('active');
+        $(body).toggleClass('fxs_push_timezone');
+        $(itemsTimezone).toggleClass('fxs_show_timezoneHours');
+    };
 
     // close all, timezone and sidebar
-    // close_timezone_sidebar.onclick = function(){
-    //     $(showTimezoneClock).removeClass('active');
-    //     $(body).removeClass('fxs_push_timezone');
-    //     $(itemsTimezone).removeClass('fxs_show_timezoneHours');
-    //     //$(body).removeClass('fxs_push_timezone');
-    //     //disableOther('close_timezone_sidebar');
-    // }
+    close_timezone_sidebar.onclick = function(){
+        $(showTimezoneClock).removeClass('active');
+        $(body).removeClass('fxs_push_timezone');
+        $(itemsTimezone).removeClass('fxs_show_timezoneHours');
+        //$(body).removeClass('fxs_push_timezone');
+        //disableOther('close_timezone_sidebar');
+    }
 
     showLeft.onclick = function () {
         $(this).toggleClass('active');
@@ -410,12 +410,12 @@ $(document).ready(function () {
 'use strict';
 /* global instantsearch */
 
-// var search = instantsearch({
-//     appId: '50DEV6P9K0',
-//     apiKey: '3805ad29c0e7ea0077d1acab07993f62',
-//     indexName: 'FxsIndexQa',
-//     urlSync: true
-// });
+var search = instantsearch({
+    appId: '50DEV6P9K0',
+    apiKey: '3805ad29c0e7ea0077d1acab07993f62',
+    indexName: 'FxsIndexQa',
+    urlSync: true
+});
 
 search.addWidget(
   instantsearch.widgets.searchBox({
